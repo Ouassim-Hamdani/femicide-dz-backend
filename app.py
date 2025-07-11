615af3209f9b801f4e3135a4d4cea5aef6fb2c61
Ouassim Hamdani <o_hamdani@estin.dz>
2025-07-11 21:42:24 +0200

    Fixed CORS

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from db import DBHandler
 from utils import convert_ids
 # Startup code
 app = FastAPI()
 
 db = DBHandler()
+
+origins = [
+    "http://localhost:3000",
+    "https://femicide.net", 
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # Allow cookies to be sent with cross-origin requests
+    allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],     # Allow all headers in the request
+)
+
 
 
 
